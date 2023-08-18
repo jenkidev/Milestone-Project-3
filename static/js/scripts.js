b2baa73027b9f72a11d9037ad19c3aade1b6e37e
jenkidev <>
2023-08-18 12:08:34 +0000

    Connected emailJS to contact form

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -6,3 +6,26 @@ $(document).ready(function(){
     $('.datepicker').datepicker();
   
 });
+
+
+/* Script for sending email through email.js*/
+const form = document.getElementById("contactForm");
+
+function sendMail(contactForm) {
+    emailjs.send("service_1wd0gcf", "template_y3nsvji",
+    {"from_name": contactForm.first_name.value + contactForm.last_name.value,
+     "from_email": contactForm.email.value,
+     "query": contactForm.query.value
+    },
+    "Iw8TFysbugKKsDqz7")
+    .then(function(response) {
+        console.log('SUCCESS!', response.status, response.text);
+        alert("Your message has been sent");
+     }, function(error) {
+        console.log('FAILED...', error);
+     });
+     setTimeout(function() {
+      form.reset();
+    }, 3000);
+     return false;
+}
