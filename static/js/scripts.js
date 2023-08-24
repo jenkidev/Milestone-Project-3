f22389ca79b3fe9c7e65f993f9c80ec3edcd4298
jenkidev <>
2023-08-24 12:52:15 +0000

    Added date script to add article to auto populate with current date

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -16,6 +16,18 @@ $(document).ready(function(){
   
 });
 
+/*Script for displaing current date in add article form code taken from the following post 
+(https://stackoverflow.com/questions/51690498/how-can-i-insert-current-date-as-a-value-in-an-input-form-field)*/
+
+var months = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+var n = new Date();
+var y = n.getFullYear();
+var m = n.getMonth();
+var d = n.getDate();
+document.getElementById("article_date").value = d + " " + months[m] + " " + y;
+
 
 // Script for sending email through email.js
 const form = document.getElementById("contactForm");
